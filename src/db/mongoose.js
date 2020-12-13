f6a90c7fd0c1118001e18a2eb3d854b525653cd1
T14g <ottiagos@gmail.com>
2020-12-12 22:47:50 -0300

    Task Model / Endpoint

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,17 +4,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
-})
-
-//Model definition
-const Task = mongoose.model('Task', {
-    description : {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed : {
-        type: Boolean,
-        default: false
-    }
-})
+});
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ require('./db/mongoose')
 //Import User Model
 const User = require('./models/user');
 
+const Task = require('./models/task');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -16,12 +18,22 @@ app.post('/users', (req, res) => {
     let user = new User(req.body);
     // console.log(user);
     user.save().then(() => {
-        res.send(user);
+        res.status(201).send(user);
     }).catch((e) => {
         res.status(400).send(e);//bad request, single line concept, chaining
     }) 
 });
 
+app.post('/tasks', (req, res) => {
+    let task = new Task(req.body);
+
+    task.save().then(() => {
+        res.status(201).send(task);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
 app.listen(port , () => {
     console.log("Server Running on Port " + port);
-});
+}); 
