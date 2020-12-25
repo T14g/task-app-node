a1a9f0442f87b1cca9bbcff9bd84e3b690aec015
T14g <ottiagos@gmail.com>
2020-12-25 18:48:40 -0300

    API Update Handlers

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ require('./db/mongoose')
 const User = require('./models/user');
 
 const Task = require('./models/task');
+const { update } = require('./models/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -71,6 +72,62 @@ app.get('/users/:id', async (req, res) => {
     }catch (e) {
         res.status(500).send();
     }
+
+})
+
+app.patch('/users/:id', async (req, res) => {
+
+    //Return an array of strings with all object properties
+    const updates = Object.keys(req.body);
+
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+
+    //Every == all or none
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if(!isValidOperation) {
+        return res.status(400).send({'error' : 'Invalid updates'});
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new : true, runValidators: true});
+
+        if(!user) {
+            return res.status(400).send();
+        }
+
+        res.send(user);
+    }catch (e) {
+        res.status(400).send(e);
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    
+    const updates = Object.keys(req.body);
+
+    const allowedUpdates = ['description', 'completed'];
+
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if(!isValidOperation) {
+        return res.status(400).send({'error' : 'Invalid updates'});
+    }
+
+    try {
+
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+
+        if(!task) {
+            return res.status(404).send();
+        }
+
+        //success
+        res.send(task);
+    }catch (e) {
+        res.status(400).send(e);
+    }
+
 
 })
 
